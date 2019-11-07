97481f3823760574bbe6b62005618591a8d194cb
Parantapa Bhattacharya <pb@parantapa.net>
2019-11-07 12:04:37 -0500

    Refactor mpi_acomm

--- xactor/mpi_acomm.py
+++ xactor/mpi_acomm.py
@@ -23,83 +23,19 @@ LOG = logging.getLogger("%s.%d" % (__name__, WORLD_RANK))
 StopAsyncReceiver = sentinel.StopAsyncReceiver
 
 
-class MessageBuffer:
-    """Buffer for pickling objects."""
-
-    def __init__(self):
-        self.buffer = []
-        self.total_size = 0
-
-    def append(self, msg):
-        """Add an object to the buffer."""
-        pkl = pickle.dumps(msg, pickle.HIGHEST_PROTOCOL)
-        pkl_len = len(pkl)
-        if pkl_len > MAX_MESSAGE_SIZE:
-            raise ValueError("Message too large %d > %d" % (pkl_len, MAX_MESSAGE_SIZE))
-
-        if self.total_size + pkl_len < MAX_MESSAGE_SIZE:
-            self.buffer.append(pkl)
-            self.total_size += pkl_len
-            return None
-
-        imsgs = self.buffer
-        self.buffer = [pkl]
-        self.total_size = pkl_len
-        return imsgs
-
-    def flush(self):
-        """Flush out the current buffer."""
-        if not self.buffer:
-            return None
-
-        imsgs = self.buffer
-        self.buffer = []
-        self.total_size = 0
-        return imsgs
-
-
-class AsyncSender:
+class AsyncRawSender:
     """Manager for sending messages."""
 
     def __init__(self):
         """Initialize."""
-        self.buffer = {rank: MessageBuffer() for rank in range(WORLD_SIZE)}
         self.pending_sends = []
 
     def send(self, to, msg):
         """Send a messge."""
-        imsgs = self.buffer[to].append(msg)
-        if imsgs is not None:
-            self.do_send(to, imsgs)
-            self.cleanup_finished_sends()
-
-    def do_send(self, to, msgs):
-        """Send all messages that have been cached."""
-        pkl = pickle.dumps(msgs, pickle.HIGHEST_PROTOCOL)
-        assert len(pkl) <= BUFFER_SIZE
-        if __debug__:
-            LOG.debug("Sending %d messages to %d", len(msgs), to)
-
-        req = COMM_WORLD.Isend([pkl, MPI.CHAR], dest=to)
+        req = COMM_WORLD.Isend([msg, MPI.CHAR], dest=to)
         self.pending_sends.append(req)
 
-    def flush(self, wait=True):
-        """Flush out message buffers."""
-        for rank in range(WORLD_SIZE):
-            msgs = self.buffer[rank].flush()
-            if msgs is not None:
-                self.do_send(rank, msgs)
-
-        if wait:
-            self.wait_pending_sends()
-        else:
-            self.cleanup_finished_sends()
-
-    def cleanup_finished_sends(self):
-        """Cleanup send requests that have already completed."""
-        if not self.pending_sends:
-            return
-
+        # Cleanup send requests that have already completed.
         indices = MPI.Request.Waitsome(self.pending_sends)
         if indices is None:
             return
@@ -109,13 +45,59 @@ class AsyncSender:
             r for i, r in enumerate(self.pending_sends) if i not in indices
         ]
 
-    def wait_pending_sends(self):
+    def close(self):
         """Wait for all pending send requests to finish."""
         if not self.pending_sends:
             return
 
         MPI.Request.Waitall(self.pending_sends)
         self.pending_sends.clear()
+
+
+class AsyncBufferedSender:
+    """Manager for sending messages."""
+
+    def __init__(self):
+        """Initialize."""
+        self.sender = AsyncRawSender()
+        self.buffer = [[] for _ in range(WORLD_SIZE)]
+        self.buffer_size = [0 for _ in range(WORLD_SIZE)]
+
+    def send(self, to, msg):
+        """Send a messge."""
+        pkl = pickle.dumps(msg, pickle.HIGHEST_PROTOCOL)
+        pkl_len = len(pkl)
+
+        if self.buffer_size[to] + pkl_len < MAX_MESSAGE_SIZE:
+            self.buffer[to].append(pkl)
+            self.buffer_size[to] += pkl_len
+            return
+
+        if __debug__:
+            LOG.debug("Sending %s messages to %d", len(self.buffer[to]), to)
+        buf = pickle.dumps(self.buffer[to], pickle.HIGHEST_PROTOCOL)
+        self.sender.send(to, buf)
+        self.buffer[to].clear()
+        self.buffer_size[to] = 0
+
+        self.buffer[to].append(pkl)
+        self.buffer_size[to] += pkl_len
+
+    def flush(self):
+        """Flush out message buffers."""
+        for to in range(WORLD_SIZE):
+            if self.buffer[to]:
+                if __debug__:
+                    LOG.debug("Sending %s messages to %d", len(self.buffer[to]), to)
+                buf = pickle.dumps(self.buffer[to], pickle.HIGHEST_PROTOCOL)
+                self.sender.send(to, buf)
+                self.buffer[to].clear()
+                self.buffer_size[to] = 0
+
+    def close(self):
+        """Flush out any remaining messages and close the sender."""
+        self.flush()
+        self.sender.close()
 
 
 class AsyncReceiver:
@@ -159,7 +141,7 @@ class AsyncCommunicator:
 
     def __init__(self):
         """Initialize."""
-        self.sender = AsyncSender()
+        self.sender = AsyncBufferedSender()
         self.receiver = AsyncReceiver()
 
         self.flush = self.sender.flush
@@ -177,7 +159,7 @@ class AsyncCommunicator:
     def finish(self):
         """Flush the sender and wait for receiver thread to finish."""
         self.sender.send(WORLD_RANK, StopAsyncReceiver)
-        self.sender.flush()
+        self.sender.close()
         self.receiver.join()
 
         qsize = self.receiver.msgq.qsize()
