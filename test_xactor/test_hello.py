87ac9408b3f8e20f8a8a2b89d51b56147cbfc217
Parantapa Bhattacharya <pb@parantapa.net>
2019-12-05 15:38:12 -0500

    Cleanup test_hello script

--- test_xactor/test_hello.py
+++ test_xactor/test_hello.py
@@ -1,24 +1,27 @@
+#!/usr/bin/env python3
 """Hello World in XActor."""
 
 import logging
 import xactor as xa
 
+# This is the class of the `Greeter` actor.
 class Greeter:
     def greet(self, name):
-        print("Greetings to %s from %s" % (xa.current_rank(), name))
+        print("Greetings to %s from rank %s" % (name, xa.current_rank()))
 
+# This the class of the `Main` actor.
 class Main:
     def main(self):
-        # Create the greeters on very rank
+        # Create a greeter on very rank.
         greeter_id = "greeter"
         for rank in xa.ranks():
             xa.create_actor(rank, greeter_id, Greeter)
 
-        # Send them the greet message
+        # Send the greeters the greet message.
         every_greeter = xa.ActorProxy(xa.EVERY_RANK, greeter_id)
         every_greeter.greet("world", send_immediate=True)
 
-        # we are done now
+        # We are done now.
         xa.stop()
 
 def test_greeter():
