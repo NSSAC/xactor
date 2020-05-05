ae2dc5f43b6546b85cab0341ab55d9c2971521cf
Parantapa Bhattacharya <pb@parantapa.net>
2020-05-04 22:05:45 -0400

    Change doc version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Rector and Visitors of the University of Virginia'
 author = 'Parantapa Bhattacharya'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = 'latest'
 
 
 # -- General configuration ---------------------------------------------------
